98c369f51508d144cef947c30ed2fed43a7311f5
isma-3788 <809562@unizar.es>
2026-04-16 12:35:47 +0200

    Arreglados problemas de conexión de la BBDD. Fin #4.

--- config.js
+++ config.js
@@ -8,9 +8,15 @@ if (!process.env.DB_HOST) console.log(`Using default DB_HOST: db`);
 if (!process.env.DB_PORT) console.log(`Using default DB_PORT: 5432`);
 
 assert(process.env.JWT_SECRET, "No JWT_SECRET provided");
+assert(process.env.DB_USER, "No DB_USER provided");
+assert(process.env.DB_PASSWORD, "No DB_PASSWORD provided");
+assert(process.env.DB_NAME, "No DB_NAME provided");
 
 module.exports = {
 	PORT: process.env.PORT || 3000,
 	DB_HOST: process.env.DB_HOST || "db",
 	DB_PORT: process.env.DB_PORT || 5432,
+	DB_USER: process.env.DB_USER,
+	DB_PASSWORD: process.env.DB_PASSWORD,
+	DB_NAME: process.env.DB_NAME,
 };
--- db.js
+++ db.js
@@ -1,9 +1,12 @@
 const { Pool } = require('pg')
-const { DB_HOST, DB_PORT } = require('./config.js')
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require('./config.js')
 
 const pool = new Pool({
+	user: DB_USER,
+	password: DB_PASSWORD,
 	host: DB_HOST,
-	port: DB_PORT
+	port: DB_PORT,
+	database: DB_NAME
 });
 
 module.exports = pool

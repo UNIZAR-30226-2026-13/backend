1e1023f9bc416c0d8e03424774dcc45276b63d38
jaimeunz <873679@unizar.es>
2026-05-05 20:01:48 +0200

    Prueba test elo. Refs #11

--- db.js
+++ db.js
@@ -1,6 +1,9 @@
 const { Pool } = require('pg')
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require('./config.js')
 
+// Base de datos de test
+const databaseName = process.env.NODE_ENV === 'test' ? 'battleship_test' : DB_NAME;
+
 const pool = new Pool({
 	user: DB_USER,
 	password: DB_PASSWORD,
